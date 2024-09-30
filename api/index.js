92daaa70baba2d4ad14d66572ec98b39d0e633c6
Aniket Bhattacharya <aniket.bhattacharya1729@gmail.com>
2024-09-30 23:03:27 +0530

    connected database to server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB') //return if connected successfully
+    })
+    .catch((err) => {
+        console.log(err) //return if any error
+    })
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
